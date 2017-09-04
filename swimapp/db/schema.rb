413b1e10540596c852f38b520a9e8166779691a2
danips2292 <danielpachesan@gmail.com>
2017-09-04 15:15:42 -0600

    entrar

--- swimapp/db/schema.rb
+++ swimapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170904043441) do
+ActiveRecord::Schema.define(version: 20170904183003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20170904043441) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "title"
+    t.text     "text"
   end
 
 end
--- swimapp/test/models/post_test.rb
+++ swimapp/test/models/post_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class NoticiaTest < ActiveSupport::TestCase
+class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
