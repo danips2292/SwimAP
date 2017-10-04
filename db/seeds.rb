ae481df835d446bae7e05da88593e4f6a0c72718
Ale Rojas <mariorojas@ic-itcr.ac.cr>
2017-10-04 13:28:24 -0600

    Seed de users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+team = Group.where(tip_group: 'Equipo')
+User.create(full_name: 'Juan Carlos Perez', email: 'email1@gmail.com'. passwword: '1234', group_id: team.id)
+User.create(full_name: 'John Marston', email: 'email2@gmail.com'. passwword: '1234', group_id: team.id)
+User.create(full_name: 'Ana Jimenez Aguilar', email: 'email3@gmail.com'. passwword: '1234', group_id: team.id)
+User.create(full_name: 'Maria Bautista Rojas', email: 'email4@gmail.com'. passwword: '1234', group_id: team.id)
+
+
+User.create(full_name: 'Juan Carlos Perez', email: 'email5@gmail.com'. passwword: '1234', group_id: 2)
+User.create(full_name: 'John Marston', email: 'email6@gmail.com'. passwword: '1234', group_id: 2)
+User.create(full_name: 'Ana Jimenez Aguilar', email: 'email7@gmail.com'. passwword: '1234', group_id: 2)
+User.create(full_name: 'Maria Bautista Rojas', email: 'email8@gmail.com'. passwword: '1234', group_id: 2)
+
+
+User.create(full_name: 'Juan Carlos Perez', email: 'email9@gmail.com'. passwword: '1234', group_id: 3)
+User.create(full_name: 'John Marston', email: 'email10@gmail.com'. passwword: '1234', group_id: 3)
+User.create(full_name: 'Ana Jimenez Aguilar', email: 'email11@gmail.com'. passwword: '1234', group_id: 3)
+User.create(full_name: 'Maria Bautista Rojas', email: 'email12@gmail.com'. passwword: '1234', group_id: 3)
