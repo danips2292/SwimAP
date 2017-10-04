56dd09dbaa7673b3e17fff62be9e4145b8f47d1c
Ale Rojas <mariorojas@ic-itcr.ac.cr>
2017-10-04 13:36:51 -0600

    Seed de users fixed 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,20 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-team = Group.where(tip_group: 'Equipo')
-User.create(full_name: 'Juan Carlos Perez', email: 'email1@gmail.com', passwword: '1234', group_id: team.id)
-User.create(full_name: 'John Marston', email: 'email2@gmail.com'. passwword: '1234', group_id: team.id)
-User.create(full_name: 'Ana Jimenez Aguilar', email: 'email3@gmail.com', passwword: '1234', group_id: team.id)
-User.create(full_name: 'Maria Bautista Rojas', email: 'email4@gmail.com', passwword: '1234', group_id: team.id)
+team = Group.where(tip_group: 'Equipo').first
+User.create(full_name: 'Juan Carlos Perez', email: 'email1@gmail.com', password: '1234', group_id: team.id)
+User.create(full_name: 'John Marston', email: 'email2@gmail.com', password: '1234', group_id: team.id)
+User.create(full_name: 'Ana Jimenez Aguilar', email: 'email3@gmail.com', password: '1234', group_id: team.id)
+User.create(full_name: 'Maria Bautista Rojas', email: 'email4@gmail.com', password: '1234', group_id: team.id)
 
 
-User.create(full_name: 'Antonia Atalanta Angelica', email: 'email5@gmail.com', passwword: '1234', group_id: 2)
-User.create(full_name: 'Carmina Leonor Patroklos', email: 'email6@gmail.com', passwword: '1234', group_id: 2)
-User.create(full_name: 'Ilithyia Rhea Dulce', email: 'email7@gmail.com', passwword: '1234', group_id: 2)
-User.create(full_name: 'Tadeo Andromeda Prudencio', email: 'email8@gmail.com' , passwword: '1234', group_id: 2)
+User.create(full_name: 'Antonia Atalanta Angelica', email: 'email5@gmail.com', password: '1234', group_id: 2)
+User.create(full_name: 'Carmina Leonor Patroklos', email: 'email6@gmail.com', password: '1234', group_id: 2)
+User.create(full_name: 'Ilithyia Rhea Dulce', email: 'email7@gmail.com', password: '1234', group_id: 2)
+User.create(full_name: 'Tadeo Andromeda Prudencio', email: 'email8@gmail.com' , password: '1234', group_id: 2)
 
 
-User.create(full_name: 'Juanita Fabián Narcissus', email: 'email9@gmail.com', passwword: '1234', group_id: 3)
-User.create(full_name: 'Luis Cristóbal Nieve', email: 'email10@gmail.com', passwword: '1234', group_id: 3)
-User.create(full_name: 'Azucena Marisol Carme', email: 'email11@gmail.com', passwword: '1234', group_id: 3)
-User.create(full_name: 'Susanita Faunus Roldán', email: 'email12@gmail.com', passwword: '1234', group_id: 3)
+User.create(full_name: 'Juanita Fabián Narcissus', email: 'email9@gmail.com', password: '1234', group_id: 3)
+User.create(full_name: 'Luis Cristóbal Nieve', email: 'email10@gmail.com', password: '1234', group_id: 3)
+User.create(full_name: 'Azucena Marisol Carme', email: 'email11@gmail.com', password: '1234', group_id: 3)
+User.create(full_name: 'Susanita Faunus Roldán', email: 'email12@gmail.com', password: '1234', group_id: 3)
