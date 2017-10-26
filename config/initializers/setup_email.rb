5d0c42a929c4e987aeea3288054e7e7f50bd82fb
Ale Rojas <mariorojas@ic-itcr.ac.cr>
2017-10-25 22:32:23 -0600

    mail funcionando local

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -4,8 +4,8 @@ ActionMailer::Base.smtp_settings =
     address: "smtp.gmail.com",
     port: 587,
     domain: "swimtec.herokuapps.com",
-    user_name: ENV["swimtecapp@gmail.com"],
-    password: ENV["itcrapp2017"],
+    user_name: "swimtecapp@gmail.com",
+    password: "itcrapp2017",
     authentication: "plain",
     openssl_verify_mode: 'none',
     enable_starttls_auto: true 
