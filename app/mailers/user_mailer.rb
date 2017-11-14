97391aa4a6969448da5e577ce6282d6c75846d8e
Ale Rojas <mariorojas@ic-itcr.ac.cr>
2017-11-13 22:13:22 -0600

    pagina de usuarios hecha y correos hechos para otras actividades

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,8 @@ class UserMailer < ActionMailer::Base
   default from: "swimtecapp@gmail.com"
   
   def new_email(email, subject, body)
-    mail to: email, subject: ""
+  	@content = body
+    mail to: email, subject: subject
   end
 
 
